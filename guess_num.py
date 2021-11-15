73a6c7759d47d8ed6ae5c782b40947b158f2e259
Ian <glps1004@gmail.com>
2021-11-15 22:17:53 +0800

    fixed the problem of incessantness and add new things

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,15 @@
 import random
 
-r = random.randint(1, 100)
+start = eval(input('請決定隨機數字範圍開始值:'))
+end = eval(input('請決定隨機數字範圍結束值:'))
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
 	num = eval(input('請猜數字:'))
 	if num == r:
 		print('你猜中了!')
+		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
