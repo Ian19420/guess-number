2b5ed71e39b41c9fafc7749c6679a3f01c9bfd5a
Ian <glps1004@gmail.com>
2022-02-19 18:37:15 +0800

    debug

--- guess_number_random_version.py
+++ guess_number_random_version.py
@@ -1,16 +1,30 @@
+def debug():
+	for _ in range(100000):
+		num = int(random.random()*99+1)
+		if num > 99 or num < 1:
+			print("there is a bug!")
+			return False
+		else:
+			return True 
 import random
 num = int(random.random()*99+1)
-times = 0
-while 1:
-	times += 1
-	ans = eval(input("your guess(1~99)?:"))
-	if ans == num:
-		print("right!")
-		print(f"you guessed {times} in total")
-		break
-	else:
-		print("wrong!")
-		if ans > num:
-			print("answer is lower than your guess.")
+x = debug()
+if x:
+	times = 0
+	while 1:
+		times += 1
+		ans = eval(input("your guess(1~99)?:"))
+		if ans == num:
+			print("right!")
+			print(f"you guessed {times} in total")
+			break
 		else:
-			print("answer is higher than your guess.")
+			print("wrong!")
+			if ans > num:
+				print("answer is lower than your guess.")
+			else:
+				print("answer is higher than your guess.")
+	else:
+		print("error")
+
+
